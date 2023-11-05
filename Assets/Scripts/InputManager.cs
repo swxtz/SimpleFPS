29fba88c2ae06026987c96cb8992e6c0a6d19552
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-05 01:30:29 -0300

    feat: initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour    
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
