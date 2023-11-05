b59d3f4bb5510367c4008a39622e824e99d673bf
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-05 01:57:55 -0300

    feat: created gravity on player motor

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,7 +6,10 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private bool isGrounded;
+
     public float speed = 5f;
+    public float gravity = -9.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
     
     // recebe os inputs do InputManager.cs e aplica no controller do player
@@ -27,5 +30,14 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
     }
 }
