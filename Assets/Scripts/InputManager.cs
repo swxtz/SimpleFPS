6c47237a70288f92329ffb3727fa6dc09c3a73f6
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-05 01:45:45 -0300

    feat: player move motor

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,18 +11,22 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
 
+    private PlayerMotor motor;
+
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //diz ao playermotor para se mover usando o valor da nossa ação de movimento
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
 
     private void OnEnable()
